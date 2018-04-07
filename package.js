560bbf690620ae8e89fa9510d667a6299fecb15b
mdranger <lizhengpeng@jingtum.com>
2018-04-07 17:52:54 -0400

    Changed the path to moaclib:chain3

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 /* jshint ignore:start */
 Package.describe({
-    name: 'moac:chain3',
-    version: '0.1.2',
+    name: 'moaclib:chain3',
+    version: '0.1.4',
     summary: 'Moac JavaScript API, middleware to talk to a ethreum node over RPC',
-    git: 'https://github.com/innowells/Chain3.git',
+    git: 'https://github.com/mdranger/chain3.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
@@ -16,18 +16,8 @@ Package.describe({
   
   Package.onUse(function(api) {
     api.versionsFrom('1.0.3.2');
-  
-    // api.use('3stack:bignumber@2.0.0', 'client');
-  
     api.export(['Chain3', 'BigNumber'], ['client', 'server']);
-  
     api.addFiles('dist/chain3.js', ['client', 'server']);
     api.addFiles('package-init.js', ['client', 'server']);
   });
   
-  // Package.onTest(function(api) {
-  //   api.use('tinytest');
-  //   api.use('test');
-  //   api.addFiles('test-tests.js');
-  // });
-  /* jshint ignore:end */
