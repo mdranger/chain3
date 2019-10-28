a2fddc80e9fa5b281bfefd6d9326521eb3b4bb57
mdranger <zhengpeng.li@moac.io>
2019-10-28 08:30:01 -0400

    prepare for 1.0.0 test, need to update docs

--- package.js
+++ package.js
@@ -1,23 +1,25 @@
 /* jshint ignore:start */
 Package.describe({
     name: 'moaclib:chain3',
-    version: '0.1.5',
-    summary: 'Moac JavaScript API, middleware to talk to a MOAC node over RPC',
-    git: 'https://github.com/mdranger/chain3.git',
+    version: '1.0.0',
+    summary: 'MOAC JavaScript API, middleware to talk to a MOAC node over RPC test version',
+    git: 'https://github.com/MOACChain/chain3',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
-  });
-  
-  Npm.depends({
+});
+
+Npm.depends({
     "xmlhttprequest": "1.7.0"
-  });
-  
-  
-  Package.onUse(function(api) {
+});
+
+
+Package.onUse(function(api) {
     api.versionsFrom('1.0.3.2');
     api.export(['Chain3', 'BigNumber'], ['client', 'server']);
     api.addFiles('dist/chain3.js', ['client', 'server']);
     api.addFiles('package-init.js', ['client', 'server']);
-  });
-  
+
+});
+
+/* jshint ignore:end */
